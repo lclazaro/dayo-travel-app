fc32a17a16b1c8e081233a4fd9f42381884ea4c4
Lejden Lazaro <lclazaro1@up.edu.ph>
2024-06-06 03:37:53 +0800

    First commit.

---
+++ travel-app/database/migrations/2024_06_05_152945_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname', length: 50);
+            $table->string('lname', length: 50);
+            $table->integer('age');
+            $table->string('sex', length: 6);
+            $table->string('contactnum', length: 11);
+            $table->string('email', length: 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
