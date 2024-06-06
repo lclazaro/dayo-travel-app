4b864db000313c79144281401e1d507528f37618
Lejden Lazaro <lclazaro1@up.edu.ph>
2024-06-06 08:42:29 +0800

    Added newlayout.php

--- travel-app/routes/web.php
+++ travel-app/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout');
+    return view('newlayout');
 });
 
 Route::resource('/users',UserController::class);
