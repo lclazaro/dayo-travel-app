f484dc30ecb5a51eb317dd67d7bfff1a6e1d8660
Poop <70053528+AnimeAddiction@users.noreply.github.com>
2024-06-06 12:35:50 +0800

    Country in progress

--- travel-app/resources/views/countries/show.blade.php
+++ travel-app/resources/views/countries/show.blade.php
@@ -3,15 +3,12 @@
  
  
 <div class="card">
-  <div class="card-header">User Page</div>
+  <div class="card-header">Country Page</div>
   <div class="card-body">
         <div class="card-body">
-        <h5 class="card-title">First Name : {{ $users->fname }}</h5>
-        <p class="card-text">Last Name : {{ $users->lname }}</p>
-        <p class="card-text">Mobile : {{ $users->age }}</p>
-        <p class="card-text">Mobile : {{ $users->sex }}</p>
-        <p class="card-text">Mobile : {{ $users->contactnum }}</p>
-        <p class="card-text">Mobile : {{ $users->email }}</p>
+        <h5 class="card-title">Country Code : {{ $countrys->country_code }}</h5>
+        <p class="card-text">Country Name : {{ $countrys->country_name }}</p>
+        <p class="card-text">Continent : {{ $countrys->continent }}</p>
   </div>
        
     </hr>
