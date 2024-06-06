f55e7b299dc2e48a353cb1435bdf541fe14e1bac
Poop <70053528+AnimeAddiction@users.noreply.github.com>
2024-06-06 11:55:00 +0800

    listing views edited

--- travel-app/resources/views/listings/edit.blade.php
+++ travel-app/resources/views/listings/edit.blade.php
@@ -5,22 +5,23 @@
   <div class="card-header">Edit Page</div>
   <div class="card-body">
       
-      <form action="{{ url('users/' .$users->id) }}" method="post">
+      <form action="{{ url('listings/' .$listings->list_id) }}" method="post">
         {!! csrf_field() !!}
         @method("PATCH")
-        <input type="hidden" name="id" id="id" value="{{$users->id}}" id="id" />
-        <label>First Name</label></br>
-        <input type="text" name="fname" id="fname" value="{{$users->fname}}" class="form-control"></br>
-        <label>Last Name</label></br>
-        <input type="text" name="lname" id="lname" value="{{$users->lname}}" class="form-control"></br>
-        <label>Age</label></br>
-        <input type="text" name="age" id="age" value="{{$users->age}}" class="form-control"></br>
-        <label>Sex</label></br>
-        <input type="text" name="sex" id="sex" value="{{$users->sex}}" class="form-control"></br>
-        <label>Contact No.</label></br>
-        <input type="text" name="contactnum" id="contactnum" value="{{$users->contactnum}}" class="form-control"></br>
-        <label>Email</label></br>
-        <input type="text" name="email" id="email" value="{{$users->email}}" class="form-control"></br>
+        <input type="hidden" name="uid" id="uid" value="{{$listings->uid}}" id="uid" />
+        <input type="hidden" name="list_id" id="list_id" value="{{$listings->list_id}}" id="list_id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$listings->name}}" class="form-control"></br>
+        <label>Unit Type</label></br>
+        <input type="text" name="unit_type" id="unit_type" value="{{$listings->unit_type}}" class="form-control"></br>
+        <label>Barangay</label></br>
+        <input type="text" name="barangay" id="barangay" value="{{$listings->barangay}}" class="form-control"></br>
+        <label>City</label></br>
+        <input type="text" name="city" id="city" value="{{$listings->city}}" class="form-control"></br>
+        <label>Province</label></br>
+        <input type="text" name="province" id="province" value="{{$listings->province}}" class="form-control"></br>
+        <label>Region</label></br>
+        <input type="text" name="region" id="region" value="{{$listings->region}}" class="form-control"></br>
         <input type="submit" value="Update" class="btn btn-success"></br>
     </form>
    
