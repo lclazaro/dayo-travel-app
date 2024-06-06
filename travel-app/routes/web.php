71c3f8d027f38a2cffca7a06c50a1d967cfa373d
Lejden Lazaro <lclazaro1@up.edu.ph>
2024-06-06 09:17:18 +0800

    Login done

--- travel-app/routes/web.php
+++ travel-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,4 @@ Route::get('/', function () {
     return view('newlayout');
 });
 
-Route::resource('/users',UserController::class);
+Route::resource('/about',AboutController::class);
